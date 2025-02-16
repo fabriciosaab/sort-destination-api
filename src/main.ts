38688f468efbdccd0d502433e6ac6a081300cc21
fabriciosaab <fabricio.saab@me.com>
2025-02-16 17:07:03 +0000

    fix POST Request

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // DTOs global validation
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
-  // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('Sort Destination API')
     .setDescription('API documentation')
@@ -17,7 +12,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
