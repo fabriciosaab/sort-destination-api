97eaead8fef40e13d538c74e8505a80371c360ca
fabriciosaab <fabricio.saab@me.com>
2025-02-16 16:31:30 +0000

    prepare to use global DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // DTOs global validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('Sort Destination API')
     .setDescription('API documentation')
